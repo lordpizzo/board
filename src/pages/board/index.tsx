e3dfb043268d22d7b1eb66cc86d541292e8866c8
lordpizzo <lordpizzo@gmail.com>
2023-01-22 15:53:02 -0300

    Edição completa
    Edição completa

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 import styles from './styles.module.scss'
-import { FiPlus, FiCalendar, FiEdit, FiTrash, FiClock } from 'react-icons/fi'
+import { FiPlus, FiCalendar, FiEdit, FiTrash, FiClock, FiX } from 'react-icons/fi'
 import { SupportButton } from '@/src/components/SupportButton'
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/react'
 import { FormEvent, useState } from 'react'
 import firesotreDB from '../../services/firebaseConnection'
-import { collection, addDoc, doc, getDoc, query, getDocs, orderBy, where, deleteDoc } from "firebase/firestore";
+import { collection, addDoc, doc, getDoc, query, getDocs, orderBy, where, deleteDoc, updateDoc } from "firebase/firestore";
 import { format } from 'date-fns'
 import Link from 'next/link'
+import { createSolutionBuilderHost } from 'typescript'
 
 type TaskList = {
 	id: string,
@@ -29,14 +30,40 @@ interface Props {
 }
 
 export default function Board({ user, dados }: Props) {
-	console.log(dados)
 	const [input, setInput] = useState('')
 	const [taskList, setTaskList] = useState<TaskList[]>(JSON.parse(dados))
+	const [taskEdit, setTaskEdit] = useState<TaskList | null>(null)
+
 	async function handleAddTask(e: FormEvent) {
 		e.preventDefault()
 		if (input === '') {
 			alert('Preencha alguma tarefa')
 			return
+		}
+
+		if (taskEdit) {
+			const tarefasCollection = collection(firesotreDB, "tarefas")
+			const tarefasDoc = doc(tarefasCollection, taskEdit.id)
+			await updateDoc(tarefasDoc,
+				{
+					tarefa: input
+				})
+				.then(() => {
+					let data = taskList
+					let taskIndex = taskList.findIndex(item => item.id === taskEdit.id)
+
+					data[taskIndex].tarefa = input
+					setTaskList(data)
+					setTaskEdit(null)
+					setInput('')
+
+				})
+				.catch((error) => {
+					console.log('Error ao atualizar', error)
+				})
+
+			return
+
 		}
 
 		await addDoc(collection(firesotreDB, "tarefas"), {
@@ -66,18 +93,25 @@ export default function Board({ user, dados }: Props) {
 
 	async function handleDelete(id: string) {
 		const tarefasCollection = collection(firesotreDB, "tarefas")
-		console.log("id da tarefa", id)
 		const tarefasDoc = doc(tarefasCollection, id)
 		await deleteDoc(tarefasDoc).then(() => {
-			console.log('Excluido com sucesso')
 			let taskDeleted = taskList.filter(item => {
 				return (item.id !== id)
 			})
 			setTaskList(taskDeleted)
 		})
-		.catch((error) => {
-			console.log('Erro ao excluir: ', error)
-		})
+			.catch((error) => {
+				console.log('Erro ao excluir: ', error)
+			})
+	}
+	async function handleEditTask(task: TaskList) {
+		setInput(task.tarefa)
+		setTaskEdit(task)
+	}
+
+	function handleCancelEdit() {
+		setInput('')
+		setTaskEdit(null)
 	}
 
 	return (
@@ -86,6 +120,14 @@ export default function Board({ user, dados }: Props) {
 				<title>Minhas tarefas</title>
 			</Head>
 			<main className={styles.container}>
+				{taskEdit && (
+					<span className={styles.warnText}>
+						<button onClick={() => handleCancelEdit()}>
+							<FiX size={30} color="#FF3636" />
+						</button>
+						Você está editando uma tarefa
+					</span>
+				)}
 				<form onSubmit={handleAddTask}>
 					<input
 						type="text"
@@ -104,25 +146,25 @@ export default function Board({ user, dados }: Props) {
 						<article className={styles.taskList} key={task.id}>
 							<Link href={`/board/${task.id}`}>
 								<p>{task.tarefa}</p>
-								</Link>
-								<div className={styles.actions}>
+							</Link>
+							<div className={styles.actions}>
+								<div>
 									<div>
-										<div>
-											<FiCalendar size={20} color="#FFB800" />
-											<time>{task.createdFormated}</time>
-										</div>
-										<button>
-											<FiEdit size={20} color="FFF" />
-											<span>Editar</span>
-										</button>
+										<FiCalendar size={20} color="#FFB800" />
+										<time>{task.createdFormated}</time>
 									</div>
-									<button onClick={() => handleDelete(task.id)}>
-										<FiTrash size={20} color="#FF3636" />
-										<span>Excluir</span>
+									<button onClick={() => handleEditTask(task)}>
+										<FiEdit size={20} color="FFF" />
+										<span>Editar</span>
 									</button>
-
 								</div>
-							
+								<button onClick={() => handleDelete(task.id)}>
+									<FiTrash size={20} color="#FF3636" />
+									<span>Excluir</span>
+								</button>
+
+							</div>
+
 						</article>
 					))}
 				</section>
