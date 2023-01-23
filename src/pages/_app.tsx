4e05c3cfdeed92131a1670020ed1984a2e054637
lordpizzo <lordpizzo@gmail.com>
2023-01-22 23:27:19 -0300

    Doar
    Finalizando processo para doar
    exbindo banner após doar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,24 @@ import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
 import '../styles/global.scss'
-
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
+/// CLIENT_ID AUiBTbQrW2samga_IJScDziFo4Hq3l7XPlPxSil6P3L8XT-JXXAjr47YZm0BDsFYnquP8viMDQLzJCZ_
+//// SECRET ENo_iUnfw_C9mi5C9yvKSgAhODsgUiruliE_Df1HReCYxtSYyhUhoTcS4uvXI_FdGAWl97hCMHaTsUsA
+////<script src="https://www.paypal.com/sdk/js?client-id=YOUR_CLIENT_ID"></script>
+const initialOptions = {
+	"client-id": "AUiBTbQrW2samga_IJScDziFo4Hq3l7XPlPxSil6P3L8XT-JXXAjr47YZm0BDsFYnquP8viMDQLzJCZ_",
+	currency: "BRL",
+	intent: 'capture'
+}
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<SessionProvider session={pageProps.session}>
-
-				<Header />
-				<Component {...pageProps} />
+				<PayPalScriptProvider options={initialOptions}>
+					<Header />
+					<Component {...pageProps} />
+				</PayPalScriptProvider>
 			</SessionProvider>
 
 		</>
--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,8 +1,16 @@
+import { PayPalButtons } from '@paypal/react-paypal-js'
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import styles from './styles.module.scss'
+import firesotreDB from '../../services/firebaseConnection'
+import { addDoc, collection, doc, setDoc } from 'firebase/firestore'
+import { useState } from 'react'
 
+
+/// CLIENT_ID AUiBTbQrW2samga_IJScDziFo4Hq3l7XPlPxSil6P3L8XT-JXXAjr47YZm0BDsFYnquP8viMDQLzJCZ_
+//// SECRET ENo_iUnfw_C9mi5C9yvKSgAhODsgUiruliE_Df1HReCYxtSYyhUhoTcS4uvXI_FdGAWl97hCMHaTsUsA
+////<script src="https://www.paypal.com/sdk/js?client-id=YOUR_CLIENT_ID"></script>
 interface Props {
 	user: {
 		name: string,
@@ -12,7 +20,20 @@ interface Props {
 }
 
 export default function Donate({ user }: Props) {
-	console.log(user)
+
+	const [vip, setVip] = useState(false)
+
+	async function handleSaveDonate() {
+		await setDoc(doc(firesotreDB, "users", user.id), {
+			donate: true,
+			lastDonate: new Date(),
+			Image: user.image
+		}).then(() => {
+			setVip(true)
+		})
+
+	}
+
 	return (
 		<>
 			<Head>
@@ -20,13 +41,36 @@ export default function Donate({ user }: Props) {
 			</Head>
 			<main className={styles.container}>
 				<img src="/images/rocket.svg" alt="Seja Apoiador" />
-				<div className={styles.vip}>
-					<img src={user.image} alt={user.name} />
-					<span>Parabéns você é um apoiador!</span>
-				</div>
+
+				{vip && (
+					<div className={styles.vip}>
+						<img src={user.image} alt={user.name} />
+						<span>Parabéns você é um apoiador!</span>
+					</div>
+				)}
+
 				<h1>Seja um apoiador deste projeto 🏆</h1>
 				<h3>Contribua com apenas <span>R$1,00</span></h3>
 				<strong>Apareça na nossa home, tenha funcionalidades exclusivas</strong>
+
+				<PayPalButtons createOrder={(data, actions) => {
+					return actions.order.create({
+						purchase_units: [{
+							amount: {
+								value: '1'
+							}
+						}]
+					})
+				}}
+					onApprove={(data, actions) => {
+						return actions.order?.capture().then(function (details) {
+							console.log('Compra aprovada: ' + details.payer.name?.given_name)
+							handleSaveDonate()
+						})
+					}}
+				>
+
+				</PayPalButtons>
 			</main>
 		</>
 	)
